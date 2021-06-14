69c1db53f0ca50c2e6118b5e29520858729a40f8
cheoljin408 <kcj409@naver.com>
2021-06-14 14:46:19 +0900

    백준 2346 풍선터뜨리

---
+++ Data Structure/2346.py
@@ -0,0 +1,23 @@
+import sys
+
+N = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split()))
+idx_list = []
+for i in range(1, N+1):
+    idx_list.append(i)
+result = []
+
+idx = 0
+num = nums.pop(idx)
+result.append(idx_list.pop(idx))
+
+while nums:
+    if num > 0:
+        idx = (idx + (num - 1)) % len(nums)
+    else:
+        idx = (idx + num) % len(nums)
+    num = nums.pop(idx)
+    result.append(idx_list.pop(idx))
+
+for i in result:
+    print(i, end=' ')
