e8bdc6cc24df0894e8f150946162012862f1c234
cheoljin408 <kcj409@naver.com>
2021-06-21 16:50:44 +0900

    programmers level2 소수찾기

---
+++ programmers/level2/소수찾기.py
@@ -0,0 +1,30 @@
+from itertools import permutations
+
+def solution(numbers):
+
+    nums = []
+    for i in range(1, len(numbers)+1):
+        num_list = list(permutations(numbers, i))
+
+        for j in set(num_list):
+            num = []
+            for k in j:
+                num.append(k)
+            nums.append(int(''.join(num)))
+
+        s_nums = set(nums)
+        answer = 0
+        for j in s_nums:
+            if j >= 3:
+                flag = True
+                for k in range(2, j):
+                    if j % k == 0:
+                        flag = False
+                        break
+                if flag:
+                    answer += 1
+                    print(answer)
+
+    return answer
+
+print(solution("011"))
