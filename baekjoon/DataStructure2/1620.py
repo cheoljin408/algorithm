b2b0f3f73a7c7c6280df442c6294f9ede82fd8db
cheoljin408 <kcj409@naver.com>
2021-06-23 19:44:13 +0900

    baekjoon 1620 포켓몬마스터

---
+++ baekjoon/DataStructure2/1620.py
@@ -0,0 +1,17 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+names = {}
+indexes = {}
+
+for i in range(1, N+1):
+    name = sys.stdin.readline().strip()
+    names[i] = name
+    indexes[name] = i
+
+for i in range(M):
+    option = sys.stdin.readline().strip()
+    if option.isdigit():
+        print(names[int(option)])
+    else:
+        print(indexes[option])
