f75702c929ea23b02d3099541a2861b085fbbdb6
cheoljin408 <kcj409@naver.com>
2021-06-30 14:44:12 +0900

    baekjoon 14425 문자열집합

--- baekjoon/DataStructure2/14425.py
+++ baekjoon/DataStructure2/14425.py
@@ -8,12 +8,9 @@ for _ in range(N):
     string = sys.stdin.readline().strip()
     strings[string] = True
 
-print(strings)
-
 for _ in range(M):
     option = sys.stdin.readline().strip()
     if option in strings:
-        print(option in strings)
         cnt += 1
 
 print(cnt)
