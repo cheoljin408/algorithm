18393b7be88c5b6b01c8cb5bf9214ebda53ea487
cheoljin408 <kcj409@naver.com>
2021-08-06 11:39:07 +0900

    programmers level1 콜라츠추측

---
+++ programmers/level1/콜라츠추측.py
@@ -0,0 +1,14 @@
+def solution(num):
+    count = 0
+    
+    while num != 1:
+        if count == 500:
+            return -1
+        num = num / 2 if num % 2 == 0 else num*3 + 1
+        count += 1
+        
+    return count
+
+print(solution(6))
+print(solution(16))
+print(solution(626331))
