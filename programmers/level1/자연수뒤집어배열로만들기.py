5b8b2f99e93f0a82924a7d87f1a886c650cf45fa
cheoljin408 <kcj409@naver.com>
2021-08-10 20:49:08 +0900

    programmers level1 자연수뒤집어배열로만들기

---
+++ programmers/level1/자연수뒤집어배열로만들기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    
+    num_list = list(str(n))
+    for i in range(len(num_list)-1, -1, -1):
+        answer.append(int(num_list[i]))
+    
+    return answer
+
+print(solution(12345))
