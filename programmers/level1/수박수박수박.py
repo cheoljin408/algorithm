4f04298be7960b21cb255e8270f84c071a12cc62
cheoljin408 <kcj409@naver.com>
2021-08-13 23:31:53 +0900

    programmers level1 수박수박수박

---
+++ programmers/level1/수박수박수박.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = []
+    
+    for i in range(n):
+        if i%2 == 0:
+            answer.append('수')
+        else:
+            answer.append('박')
+    
+    return ''.join(answer)
+
+print(solution(3))
+print(solution(4))
