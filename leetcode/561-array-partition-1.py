ca958be31e07b50489ae8af619879f59608aeb79
cheoljin408 <kcj409@naver.com>
2022-02-02 13:11:06 +0900

    leetcode 561 array-partition-1

--- leetcode/561-array-partition-1.py
+++ leetcode/561-array-partition-1.py
@@ -3,18 +3,22 @@ from typing import List
 
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        print(nums)
-        nums.sort()
-        print(nums)
+        # 풀이 1
+        # print(nums)
+        # nums.sort()
+        # print(nums)
+        #
+        # sum = 0
+        # pair = []
+        # for num in nums:
+        #     pair.append(num)
+        #     if len(pair) == 2:
+        #         sum += min(pair)
+        #         pair = []
+        # return sum
 
-        sum = 0
-        pair = []
-        for num in nums:
-            pair.append(num)
-            if len(pair) == 2:
-                sum += min(pair)
-                pair = []
-        return sum
+        # 풀이 2
+        return sum(sorted(nums)[::2])
 
 solution = Solution()
 print(solution.arrayPairSum([1,4,3,2]))
