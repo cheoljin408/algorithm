1a1384e0ee5aea6981fa0dc9e7e7b07ce0c72ab4
cheoljin408 <kcj409@naver.com>
2022-02-07 00:52:28 +0900

    baekjoon 2164 카드2 복습

--- baekjoon/DataStructure/2164.py
+++ baekjoon/DataStructure/2164.py
@@ -1,13 +1,34 @@
+# import sys
+# from collections import deque
+#
+# N = int(sys.stdin.readline())
+#
+# queue = deque(i+1 for i in range(N))
+#
+# while(len(queue) > 1):
+#     queue.popleft()
+#     tmp = queue.popleft()
+#     queue.append(tmp)
+#
+# print(queue[0])
+
 import sys
 from collections import deque
 
 N = int(sys.stdin.readline())
 
-queue = deque(i+1 for i in range(N))
+stack = deque(i for i in range(1, N+1))
 
-while(len(queue) > 1):
-    queue.popleft()
-    tmp = queue.popleft()
-    queue.append(tmp)
 
-print(queue[0])
+while True:
+    if len(stack) == 1:
+        break
+
+    stack.popleft()
+    if len(stack) == 1:
+        break
+
+    num = stack.popleft()
+    stack.append(num)
+
+print(stack[0])
