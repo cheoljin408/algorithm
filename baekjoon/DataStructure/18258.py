6fd24e93d7de4db824a4f41247a5b251acac3d6e
cheoljin408 <kcj409@naver.com>
2022-02-06 21:03:58 +0900

    큐 다시 풀기

--- baekjoon/DataStructure/18258.py
+++ baekjoon/DataStructure/18258.py
@@ -1,71 +1,131 @@
+# import sys
+#
+# class Queue:
+#     # INIT
+#     def __init__(self):
+#         self.queue = []
+#         self.idx = 0
+#
+#     # IS EMPTY
+#     def isEmpty(self):
+#         if len(self.queue) == self.idx:
+#             self.idx = 0
+#             self.queue = []
+#             return 1
+#         else:
+#             return 0
+#
+#     # PUSH
+#     def push(self, item):
+#         self.queue.append(item)
+#         print(self.queue)
+#
+#     # POP
+#     def pop(self):
+#         if len(self.queue) > self.idx:
+#             self.idx += 1
+#             return(self.queue[self.idx - 1])
+#         else:
+#             return -1
+#
+#     # SIZE
+#     def size(self):
+#         return len(self.queue) - self.idx
+#
+#     # FRONT
+#     def front(self):
+#         if len(self.queue) > self.idx:
+#             return self.queue[self.idx]
+#         else:
+#             return -1
+#
+#     # BACK
+#     def back(self):
+#         if len(self.queue) > self.idx:
+#             return self.queue[-1]
+#         else:
+#             return -1
+#
+# queue = Queue()
+# N = int(input())
+#
+# for _ in range(N):
+#     option = sys.stdin.readline().split()
+#
+#     if option[0] == 'push':
+#         queue.push(option[1])
+#
+#     elif option[0] == 'pop':
+#         print(queue.pop())
+#
+#     elif option[0] == 'size':
+#         print(queue.size())
+#
+#     elif option[0] == 'empty':
+#         print(queue.isEmpty())
+#
+#     elif option[0] == 'front':
+#         print(queue.front())
+#
+#     elif option[0] == 'back':
+#         print(queue.back())
+
 import sys
 
-class Queue:
-    # INIT
-    def __init__(self):
-        self.queue = []
-        self.idx = 0
+def push(queue, item):
+    queue.append(item)
 
-    # IS EMPTY
-    def isEmpty(self):
-        if len(self.queue) == self.idx:
-            self.idx = 0
-            self.queue = []
-            return 1
-        else:
-            return 0
+def pop(queue, front_idx):
+    if len(queue) <= front_idx:
+        print(-1)
+        return
+    print(queue[front_idx])
 
-    # PUSH
-    def push(self, item):
-        self.queue.append(item)
-        print(self.queue)
+def size(queue, front_idx):
+    print(len(queue)-front_idx)
 
-    # POP
-    def pop(self):
-        if len(self.queue) > self.idx:
-            self.idx += 1
-            return(self.queue[self.idx - 1])
-        else:
-            return -1
+def empty(queue, front_idx):
+    if len(queue) == front_idx:
+        print(1)
+        return
+    print(0)
 
-    # SIZE
-    def size(self):
-        return len(self.queue) - self.idx
+def front(queue, front_idx):
+    if len(queue) == front_idx:
+        print(-1)
+        return
+    print(queue[front_idx])
 
-    # FRONT
-    def front(self):
-        if len(self.queue) > self.idx:
-            return self.queue[self.idx]
-        else:
-            return -1
+def back(queue, front_idx):
+    if len(queue) == front_idx:
+        print(-1)
+        return
+    print(queue[len(queue)-1])
 
-    # BACK
-    def back(self):
-        if len(self.queue) > self.idx:
-            return self.queue[-1]
-        else:
-            return -1
 
-queue = Queue()
-N = int(input())
+N = int(sys.stdin.readline())
+front_idx = 0
+queue = []
 
 for _ in range(N):
-    option = sys.stdin.readline().split()
-    
-    if option[0] == 'push':
-        queue.push(option[1])
+    operation = sys.stdin.readline().split()
 
-    elif option[0] == 'pop':
-        print(queue.pop())
+    if operation[0] == 'push':
+        push(queue, operation[1])
 
-    elif option[0] == 'size':
-        print(queue.size())
-    
-    elif option[0] == 'empty':
-        print(queue.isEmpty())
-    
-    elif option[0] == 'front':
-        print(queue.front())
+    elif operation[0] == 'pop':
+        pop(queue, front_idx)
+        if len(queue) > front_idx:
+            front_idx += 1
 
-    elif option[0] == 'back':
-        print(queue.back())
+    elif operation[0] == 'size':
+        size(queue, front_idx)
+
+    elif operation[0] == 'empty':
+        empty(queue, front_idx)
+
+    elif operation[0] == 'front':
+        front(queue, front_idx)
+
+    elif operation[0] == 'back':
+        back(queue, front_idx)
