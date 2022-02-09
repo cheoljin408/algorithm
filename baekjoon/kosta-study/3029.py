9e74c5567753f81196d28149271c9152eda38246
cheoljin408 <kcj409@naver.com>
2022-02-09 23:44:17 +0900

    baekjoon 3029 경고 복습

--- baekjoon/kosta-study/3029.py
+++ baekjoon/kosta-study/3029.py
@@ -1,9 +1,16 @@
-h1, m1, s1 = map(int, input().split(':'))
-h2, m2, s2 = map(int, input().split(':'))
-t1 = h1*60*60 + m1*60 + s1
-t2 = h2*60*60 + m2*60 + s2
-t = t2 - t1 if t2 > t1 else t2-t1+24*60*60
-h = t//60//60
-m = t//60 % 60
-s = t%60
-print("%02d:%02d:%02d" % (h, m, s))
+a1, b1, c1 = map(int, input().split(":"))
+a2, b2, c2 = map(int, input().split(":"))
+
+ans1 = a1 * 3600 + b1 * 60 + c1
+ans2 = a2 * 3600 + b2 * 60 + c2
+
+if ans1 >= ans2:
+    ans2 += 3600 * 24
+
+ans3 = ans2 - ans1
+a3 = ans3 // 3600
+ans3 %= 3600
+b3 = ans3 // 60
+ans3 %= 60
+c3 = ans3
+print("%02d:%02d:%02d" % (a3, b3, c3))
