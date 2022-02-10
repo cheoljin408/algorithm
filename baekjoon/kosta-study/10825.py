c9195f9a4edf7fd5dc3db5b745cb99241a4cd0dc
cheoljin408 <kcj409@naver.com>
2022-02-11 00:07:36 +0900

    baekjoon 10825 국영수

---
+++ baekjoon/kosta-study/10825.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+scores = [input().split() for _ in range(N)]
+
+scores.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in range(N):
+    print(scores[i][0])
