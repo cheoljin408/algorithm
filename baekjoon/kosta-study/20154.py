8b5bffcfaffc85230f25ea640f4a3749b242bf78
cheoljin408 <kcj409@naver.com>
2022-02-14 01:37:57 +0900

    baekjoon 20154

---
+++ baekjoon/kosta-study/20154.py
@@ -0,0 +1,38 @@
+import sys
+
+draw_count = {
+    'A': 3,
+    'B': 2,
+    'C': 1,
+    'D': 2,
+    'E': 3,
+    'F': 3,
+    'G': 3,
+    'H': 3,
+    'I': 1,
+    'J': 1,
+    'K': 3,
+    'L': 1,
+    'M': 3,
+    'N': 3,
+    'O': 1,
+    'P': 2,
+    'Q': 2,
+    'R': 2,
+    'S': 1,
+    'T': 2,
+    'U': 1,
+    'V': 1,
+    'W': 2,
+    'X': 2,
+    'Y': 2,
+    'Z': 1
+}
+
+s = list(sys.stdin.readline())
+s.pop()
+sum = 0
+for i in s:
+    sum += draw_count[i]
+
+print("I'm a winner!") if sum%2 == 1 else print("You're the winner?")
