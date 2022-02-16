6a0f4e98a20ca963d655e029fb11a56e1d08776c
cheoljin408 <kcj409@naver.com>
2022-02-16 23:17:08 +0900

    baekjoon 1874 스택수열 복습

--- baekjoon/DataStructure/1874.py
+++ baekjoon/DataStructure/1874.py
@@ -1,33 +1,66 @@
+# import sys
+#
+# N = int(sys.stdin.readline())
+# nums = []
+# plus_num = 0
+# stck = []
+# answer = []
+# flag = True
+#
+# for i in range(N):
+#     nums.append(int(sys.stdin.readline()))
+#
+# for i in nums:
+#     while True:
+#         if i <= plus_num:
+#             break
+#         plus_num += 1
+#         stck.append(plus_num)
+#         answer.append('+')
+#     # print(stck)
+#     # print(answer)
+#
+#     if i == stck[-1]:
+#         stck.pop()
+#         answer.append('-')
+#
+#     else:
+#         flag = False
+#
+# if not flag:
+#     print('NO')
+# else:
+#     print('\n'.join(answer))
+
 import sys
 
+num_list = []
+
 N = int(sys.stdin.readline())
-nums = []
-plus_num = 0
-stck = []
+for _ in range(N):
+    num_list.append(int(sys.stdin.readline()))
+
+count = 0
+stack = []
 answer = []
 flag = True
-
-for i in range(N):
-    nums.append(int(sys.stdin.readline()))
-
-for i in nums:
+for num in num_list:
     while True:
-        if i <= plus_num:
+        if num == count:
             break
-        plus_num += 1
-        stck.append(plus_num)
+        count += 1
+        stack.append(count)
         answer.append('+')
-    # print(stck)
-    # print(answer)
 
-    if i == stck[-1]:
-        stck.pop()
+    if stack[-1] == num:
+        stack.pop()
         answer.append('-')
-    
     else:
         flag = False
 
 if not flag:
     print('NO')
 else:
-    print('\n'.join(answer))
+    for a in answer:
+        print(a)
+
