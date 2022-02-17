a0bb461c3240dadf84467c89f9eccd4c179877e2
cheoljin408 <kcj409@naver.com>
2022-02-17 23:39:12 +0900

    baekjoon 1935 후위표기식2 복습

--- baekjoon/DataStructure/1935.py
+++ baekjoon/DataStructure/1935.py
@@ -1,37 +1,73 @@
+# import sys
+#
+# N = int(sys.stdin.readline())
+# string = list(sys.stdin.readline())
+# nums = []
+# for _ in range(N):
+#     nums.append(int(sys.stdin.readline()))
+#
+# for idx, val in enumerate(string):
+#     if val >= 'A' and val <= 'Z':
+#         string[idx] = nums[ord(val) - ord('A')]
+#
+# len_string = len(string)
+#
+# stck = []
+#
+# for i in string[:len_string-1]:
+#     if i == '+':
+#         num1 = stck.pop()
+#         num2 = stck.pop()
+#         stck.append(num2 + num1)
+#     elif i == '-':
+#         num1 = stck.pop()
+#         num2 = stck.pop()
+#         stck.append(num2 - num1)
+#     elif i == '*':
+#         num1 = stck.pop()
+#         num2 = stck.pop()
+#         stck.append(num2 * num1)
+#     elif i == '/':
+#         num1 = stck.pop()
+#         num2 = stck.pop()
+#         stck.append(num2 / num1)
+#     else:
+#         stck.append(i)
+#
+# print("%.2f" % (stck[0]))
+
 import sys
 
 N = int(sys.stdin.readline())
-string = list(sys.stdin.readline())
+formula = list(sys.stdin.readline())
 nums = []
 for _ in range(N):
     nums.append(int(sys.stdin.readline()))
 
-for idx, val in enumerate(string):
-    if val >= 'A' and val <= 'Z':
-        string[idx] = nums[ord(val) - ord('A')]
+idx = 0
+for i in range(len(formula)):
+    if formula[i].isalpha():
+        formula[i] = nums[ord(formula[i]) - ord('A')]
 
-len_string = len(string)
-
-stck = []
-
-for i in string[:len_string-1]:
-    if i == '+':
-        num1 = stck.pop()
-        num2 = stck.pop()
-        stck.append(num2 + num1)
-    elif i == '-':
-        num1 = stck.pop()
-        num2 = stck.pop()
-        stck.append(num2 - num1)
-    elif i == '*':
-        num1 = stck.pop()
-        num2 = stck.pop()
-        stck.append(num2 * num1)
-    elif i == '/':
-        num1 = stck.pop()
-        num2 = stck.pop()
-        stck.append(num2 / num1)
-    else:
-        stck.append(i)
-
-print("%.2f" % (stck[0]))
+formula.pop()
+stack = []
+for char in formula:
+    if isinstance(char, int):
+        stack.append(char)
+    elif char == '+':
+        num1 = stack.pop()
+        num2 = stack.pop()
+        stack.append(num2 + num1)
+    elif char == '-':
+        num1 = stack.pop()
+        num2 = stack.pop()
+        stack.append(num2 - num1)
+    elif char == '*':
+        num1 = stack.pop()
+        num2 = stack.pop()
+        stack.append(num2 * num1)
+    elif char == '/':
+        num1 = stack.pop()
+        num2 = stack.pop()
+        stack.append(num2 / num1)
+print("%.2f" % (stack[0]))
