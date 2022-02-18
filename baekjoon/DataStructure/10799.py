8bbc8f2a94899e44b2e0dab4d689a262b7b0d9fd
cheoljin408 <kcj409@naver.com>
2022-02-18 23:35:37 +0900

    baekjoon 10799 쇠막대기 복습

--- baekjoon/DataStructure/10799.py
+++ baekjoon/DataStructure/10799.py
@@ -1,18 +1,38 @@
+# import sys
+#
+# string = list(sys.stdin.readline())
+# sum = 0
+# cut_left = 0
+# cut_right = 1
+#
+# for idx, item in enumerate(string):
+#     if item == '(':
+#         cut_left += 1
+#     elif item == ')':
+#         cut_left -= 1
+#         if string[idx-1] == '(':
+#             sum += cut_left
+#         else:
+#             sum += cut_right
+#
+# print(sum)
+
 import sys
 
-string = list(sys.stdin.readline())
+stick = list(sys.stdin.readline())
+stick.pop()
 sum = 0
-cut_left = 0
-cut_right = 1
-
-for idx, item in enumerate(string):
-    if item == '(':
-        cut_left += 1
-    elif item == ')':
-        cut_left -= 1
-        if string[idx-1] == '(':
-            sum += cut_left
+left = 0
+right = 1
+for i, e in enumerate(stick):
+    if e == '(':
+        left += 1
+    else:
+        left -= 1
+        if stick[i-1] == '(':
+            sum += left
         else:
-            sum += cut_right
+            sum += right
 
 print(sum)
+
