77c2a4f2b96eedec3199f8b6c98fb3b0c002ea16
cheoljin408 <kcj409@naver.com>
2022-02-20 23:49:21 +0900

    baekjoon 1966 프린터 큐 복습

--- baekjoon/DataStructure/1966.py
+++ baekjoon/DataStructure/1966.py
@@ -1,3 +1,27 @@
+# import sys
+#
+# case_num = int(sys.stdin.readline())
+#
+# for _ in range(case_num):
+#     N, M = map(int, (sys.stdin.readline().split()))
+#     priority = list(map(int, sys.stdin.readline().split()))
+#     idx = [False for _ in range(N)]
+#     idx[M] = True
+#     count = 0
+#
+#     while True:
+#         if priority[0] != max(priority):
+#             priority.append(priority.pop(0))
+#             idx.append(idx.pop(0))
+#         else:
+#             if idx[0]:
+#                 print(count)
+#                 break
+#             else:
+#                 count += 1
+#                 priority.pop(0)
+#                 idx.pop(0)
+
 import sys
 
 case_num = int(sys.stdin.readline())
@@ -21,3 +45,4 @@ for _ in range(case_num):
                 count += 1
                 priority.pop(0)
                 idx.pop(0)
+
