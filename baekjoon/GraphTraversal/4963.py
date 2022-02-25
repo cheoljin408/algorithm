41ec52c16af617d057d4eb781b56880ab3e7c2af
cheoljin408 <kcj409@naver.com>
2022-02-26 00:51:09 +0900

    baekjoon 4963 섬의개수 복습

--- baekjoon/GraphTraversal/4963.py
+++ baekjoon/GraphTraversal/4963.py
@@ -1,9 +1,52 @@
+# import sys
+# sys.setrecursionlimit(100000)
+# def dfs(x, y):
+#     if x <= -1 or x >= M or y <= -1 or y >= N:
+#         return False
+#
+#     if graph[x][y] == 1:
+#         graph[x][y] = 0
+#
+#         dfs(x - 1, y)
+#         dfs(x, y - 1)
+#         dfs(x + 1, y)
+#         dfs(x, y + 1)
+#
+#         dfs(x - 1, y - 1)
+#         dfs(x - 1, y + 1)
+#         dfs(x + 1, y - 1)
+#         dfs(x + 1, y + 1)
+#
+#         return True
+#     return False
+#
+# while True:
+#     N, M = map(int, input().split())
+#     if N == 0 and M == 0:
+#         break
+#
+#     graph = []
+#     for i in range(M):
+#         graph.append(list(map(int, input().split())))
+#
+#     result = 0
+#
+#     for i in range(M):
+#         for j in range(N):
+#             if dfs(i, j) == True:
+#                 result += 1
+#
+#     print(result)
+
 import sys
+
 sys.setrecursionlimit(100000)
+
+
 def dfs(x, y):
     if x <= -1 or x >= M or y <= -1 or y >= N:
         return False
-    
+
     if graph[x][y] == 1:
         graph[x][y] = 0
 
@@ -19,6 +62,7 @@ def dfs(x, y):
 
         return True
     return False
+
 
 while True:
     N, M = map(int, input().split())
