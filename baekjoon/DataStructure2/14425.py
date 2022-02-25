ccd17bee43685d06f34ca573fd8d0fe8b1100658
cheoljin408 <kcj409@naver.com>
2022-02-25 11:08:12 +0900

    baekjoon 14425 문자열집합 복습

--- baekjoon/DataStructure2/14425.py
+++ baekjoon/DataStructure2/14425.py
@@ -1,3 +1,20 @@
+# import sys
+#
+# N, M = map(int, sys.stdin.readline().split())
+# strings = {}
+# cnt = 0
+#
+# for _ in range(N):
+#     string = sys.stdin.readline().strip()
+#     strings[string] = True
+#
+# for _ in range(M):
+#     option = sys.stdin.readline().strip()
+#     if option in strings:
+#         cnt += 1
+#
+# print(cnt)
+
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
