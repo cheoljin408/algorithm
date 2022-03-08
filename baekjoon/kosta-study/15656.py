0440530e8d22c99299533fdaee3f61ac41de27c5
cheoljin408 <kcj409@naver.com>
2022-03-09 00:09:09 +0900

    baekjoon 15655 N과 M(7)

---
+++ baekjoon/kosta-study/15656.py
@@ -0,0 +1,12 @@
+import sys
+import itertools
+
+N, M = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+
+nums.sort()
+c_nums = itertools.product(nums, repeat=M)
+for i in c_nums:
+    for j in range(M):
+        print(i[j], end=' ')
+    print()
