638f8e83cf19e065b09f9232ff6fcd59d327458f
cheoljin408 <kcj409@naver.com>
2022-03-10 20:56:53 +0900

    baekjoon 15664 N과 M(12)

---
+++ baekjoon/kosta-study/15666.py
@@ -0,0 +1,13 @@
+import sys
+import itertools
+
+N, M = map(int, sys.stdin.readline().split())
+nums = list(map(int, sys.stdin.readline().split()))
+
+nums.sort()
+c_nums = itertools.combinations_with_replacement(nums, M)
+
+for i in sorted(list(set(c_nums))):
+    for j in range(M):
+        print(i[j], end=' ')
+    print()
